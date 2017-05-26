f2dd0f0a246cdcf05c306ba13b4d85f99d6cf970
lottebijl <onlinedept>
2017-05-26 23:55:32 +0200

    content-block + instafeed

--- app/scripts/modules.js
+++ app/scripts/modules.js
@@ -10,7 +10,8 @@ function getInstagramFeed(){
         userId: user,
         clientId:'3f82142d11d34cef993bfb28ed7999bf',
         accessToken:accessCode,
-        template: '<div class="columns small-24 large-8"><div class="instafeed__item"><a href="{{link}}"><img src="{{image}}" /></a><div class="instafeed__content"><span>{{likes}} likes</span><p>{{caption}}</p></div></div></div>',
+        resolution:'standard_resolution',
+        template: '<div class="columns small-24 medium-12 large-8 end"><div class="instafeed__item"><a href="{{link}}"><img src="{{image}}" /></a><div class="instafeed__content"><p>{{caption}}</p></div></div></div>',
         after: function() {
             console.log('after');
         },
