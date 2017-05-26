8b64a23098905ab3cec45b24136b302ab3f013aa
lottebijl <onlinedept>
2017-05-26 21:43:38 +0200

    mobile menu + topnavigation

--- dist/js/script.js
+++ dist/js/script.js
@@ -26,3 +26,21 @@ getInstagramFeed();
 
 
 
+function Menu(){
+  var menuBtn = $('.btn-menu');
+  var mobileMenu = $('.mobile-menu');
+  menuBtn.click(function(event) {
+    event.preventDefault();
+    mobileMenu.toggleClass('open');
+    menuBtn.toggleClass('close');
+    if (mobileMenu.attr('class') == 'open'){
+      mobileMenu.on('touchmove', function(event) {
+        event.preventDefault();
+      });
+    }
+  });
+
+}
+
+Menu();
+
