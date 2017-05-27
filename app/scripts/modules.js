c88ee0887f83df08ce81090dff9d23452cf7fd26
lottebijl <onlinedept>
2017-05-27 16:05:26 +0200

    finish form validation

--- app/scripts/modules.js
+++ app/scripts/modules.js
@@ -1,4 +1,20 @@
 $(document).foundation();
+
+$(".contact__form").on("submit", function(ev) {
+  ev.preventDefault();
+});
+
+$(".contact__form").on("formvalid.zf.abide", function(ev,frm) {
+  $('.alert.success').show();
+  $('.contact__fields').hide();
+  $('.btn-submit').hide();
+});
+
+$(".contact__form").on('change', 'input, textarea', function(){
+  if($(this).prop('required')){
+    $(this).addClass('dirty');
+  }
+});
 function getInstagramFeed(){
   var accessStr   = window.location.hash.substr(1);
   var accessCode  = accessStr.replace(/access_token=/g,'');
--- app/scripts/modules/form-validation.js
+++ app/scripts/modules/form-validation.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+$(".contact__form").on("submit", function(ev) {
+  ev.preventDefault();
+});
+
+$(".contact__form").on("formvalid.zf.abide", function(ev,frm) {
+  $('.alert.success').show();
+  $('.contact__fields').hide();
+  $('.btn-submit').hide();
+});
+
+$(".contact__form").on('change', 'input, textarea', function(){
+  if($(this).prop('required')){
+    $(this).addClass('dirty');
+  }
+});
